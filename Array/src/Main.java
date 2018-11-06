26336d362427d4a4542833719f6e33f89b7d6373
fengtongyong <1109091027@qq.com>
2018-11-06 17:04:14 +0800

    提交项目

---
+++ Array/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] scores = new int[]{100, 99, 92};
+        for (int score : scores) {
+            System.out.println(score);
+        }
+    }
+}
